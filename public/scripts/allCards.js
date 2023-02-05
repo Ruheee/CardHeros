7cf2276d9b396fe9e2c483219388dfbfbc1e1415
Rui Dantas <ruidantas55@gmail.com>
2023-02-05 17:28:14 +0000

    Add card styling

--- public/scripts/allCards.js
+++ public/scripts/allCards.js
@@ -4,11 +4,11 @@ $(document).ready(() => {
       url: '/api/cards',
     })
     .done((response) => {
-      const $ALLCARDS = $('#all-cards');
+      const $ALLCARDS = $('#cards');
       $ALLCARDS.empty();
 
       for (let card of response.cards) {
-        $(`<li><img src="${card.card_front_url}"></li>`).appendTo($ALLCARDS);
+        $(`<div class="card"><div class="card-img" style="background-image: url('${card.card_front_url}');"></div></div>`).appendTo($ALLCARDS);
       }
     });
 
