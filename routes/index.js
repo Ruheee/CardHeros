856ba5321abc963d5ca6f00c40af6d49e2ec1051
davique0 <davique@gmail.com>
2023-02-07 20:24:47 -0500

    Add index route

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const { query } = require('express');
+const express = require('express');
+const router  = express.Router();
+
+
+const hotCardsQuery = require('../db/queries/hot-cards')
+
+
+router.get('/', (req, res) => {
+
+  hotCardsQuery.getHotCards()
+    .then(result => {
+      const userID = req.session.user_id
+      const templateVars = { userID, result }
+      res.render('index', templateVars);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+
+
+});
+
+module.exports = router;
