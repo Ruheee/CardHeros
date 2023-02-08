1f05a0db64237566b3a83c328a2c8940da6bc965
Pedro Lorandi <hello@pedrolorandi.com>
2023-02-08 11:04:00 -0500

    Fix bug of new message to message-iframe

--- routes/newMessage-frame.js
+++ routes/newMessage-frame.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router  = express.Router();
 const userByCardIDQueries = require ('../db/queries/userByCardId');
 const cardQueries = require ('../db/queries/getCard');
+const convoQueries = require ('../db/queries/conversations');
 
 router.get('/:id', (req, res) => {
+  const userID = req.session.user_id;
   const cardID = req.params.id;
   const templateVars = {};
+  let messageID;
 
   const queryArr = [ userByCardIDQueries.getUserByCardID(cardID), cardQueries.getCard(cardID) ];
 
-  Promise.all(queryArr).then((values) => {
+  Promise.all(queryArr)
+  .then((values) => {
 
     for (result of values) {
       if ('name' in result[0]) {
@@ -18,11 +22,19 @@ router.get('/:id', (req, res) => {
         templateVars.card = result;
       }
     }
-
-  })
+    Promise.all([ convoQueries.getConversations( userID, cardID ) ])
+    .then((values) => {
+      // If there is an existing convo, render the regular message
+      if(values[0].length > 0){
+        messageID = values[0][0].id;
+        console.log(messageID)
+      }
+    })
     .then((result) => {
-      res.render('ch_new_message', templateVars);
+      console.log(messageID)
+      messageID ? res.redirect(`/message/${messageID}`) : res.render('ch_new_message', templateVars);
     });
+  })
 });
 
 module.exports = router;
