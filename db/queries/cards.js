4af7e0bd74290f7356686c0bf633e784b1d69869
Pedro Lorandi <hello@pedrolorandi.com>
2023-02-08 21:43:44 -0500

    Add header and footer to pages

--- db/queries/cards.js
+++ db/queries/cards.js
@@ -26,34 +26,3 @@ const getCards = ( filters ) => {
 };
 
 module.exports = { getCards };
-
-// const getAllCards = (pageNumber) => {
-//   const LIMIT = 8;
-//   const OFFSET = pageNumber * LIMIT;
-//   return db
-//   .query(`SELECT *
-//   FROM cards
-//   LIMIT $1
-//   OFFSET $2;`, [ LIMIT, OFFSET ])
-//     .then(data => {
-//       return data.rows;
-//     });
-// };
-
-// // Function query to return single sport after pressing load more cards
-// const getAllCardsBySports = (sport, pageNumber) => {
-//   const LIMIT = 8;
-//   const OFFSET = pageNumber * LIMIT;
-//   const sportArr = sport;
-//   return db
-//   .query(`SELECT *
-//   FROM cards
-//   OR sport = $3
-//   LIMIT $1
-//   OFFSET $2;`, [ LIMIT, OFFSET, sportArr ])
-//     .then(data => {
-//       return data.rows;
-//     });
-// };
-
-// module.exports = { getAllCards, getAllCardsBySports };
