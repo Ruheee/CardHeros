a32ee1bd352873d1db6f84a7f7bcb6f3e8322f0f
Rui Dantas <ruidantas55@gmail.com>
2023-02-09 02:41:11 +0000

    Add CSS styling to have card flip to back photo

--- routes/cards_id.js
+++ routes/cards_id.js
@@ -2,20 +2,19 @@ const express = require('express');
 const router  = express.Router();
 const getCardById = require('../db/queries/getCardByID');
 const checkFavourite = require('../db/queries/addNewFav');
-const sendMessage = require('../db/queries/sendMessage')
 
 
 
 router.get('/:id', (req, res) => {
-
-  const cardID = req.params.id
-  const userID = req.session.user_id
-  let templateVars = { userID }
-  const queryArr = [getCardById.getCardById(cardID), checkFavourite.checkFavourite(userID, cardID)]
+  const cardID = req.params.id;
+  const userID = req.session.user_id;
+  let templateVars = { userID };
+  const queryArr = [getCardById.getCardById(cardID), checkFavourite.checkFavourite(userID, cardID)];
   Promise.all(queryArr)
   .then((response) => {
+    console.log(queryArr)
     templateVars['cards'] = response[0];
-    templateVars['favourites'] = response[1]
+    templateVars['favourites'] = response[1];
     res.render('ch_show_card', templateVars);
   })
 })
